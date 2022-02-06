be38db064ffdbcb2326351531ca85ad194e353bb
Nathan Kola <watersincllc@gmail.com>
2022-02-05 22:58:30 -0600

    add transact

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export const Balance = () => {
-  return 
-  <>
+  return (
+    <>
       <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
-    
-  </>;
+      <h1>$0.00</h1>
+
+    </>
+  )
 };
+
